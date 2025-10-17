2cf6983f845e766602957af465fd363b8b5843b2
Achok-kot <p.kot@alustudent.com>
2025-10-17 08:46:53 -0700

    opo

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i in (101, 113):  # skip 'e' (101) and 'q' (113)
-        continue
-    print(f"{i:c}", end="")
-
+for c in range(97, 123):
+  if c != 101 and c != 133:
+    print("{}".format(chr(c)),end="")
