6295f5a0cd6dd35a20ad278b75c779ca1974eb56
Achok-kot <p.kot@alustudent.com>
2025-11-11 16:04:28 +0200

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a square with a private size attribute.
-        
+
         Args:
             size: The size of the square (default 0).
         """
@@ -16,3 +16,4 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
