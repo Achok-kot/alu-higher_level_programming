8924c09ab6781d5f375c460a35fb58aa8faa7812
Achok-kot <p.kot@alustudent.com>
2025-11-14 18:38:28 +0200

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a MyList class that inherits from list."""
+
+
+class MyList(list):
+    """A list subclass with additional functionality to print sorted content."""
+    
+    def print_sorted(self):
+        """Print the list in sorted order without modifying the original list."""
+        print(sorted(self))
