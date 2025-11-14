060becabcb6839ddeea221557651ce1181917594
Achok-kot <p.kot@alustudent.com>
2025-11-14 19:23:44 +0200

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class that inherits from Rectangle."""
+    
+    def __init__(self, size):
+        """Initialize a Square with size.
+        
+        Args:
+            size (int): The size of the square (both width and height)
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
