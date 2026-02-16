75f8c0adb3d355fba4dd2bb7fe9099da1ba5496c
Achok-kot <p.kot@alustudent.com>
2026-02-16 19:51:26 +0200

    web scrabing

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes('/18/')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completed[todo.userId]) {
+          completed[todo.userId]++;
+        } else {
+          completed[todo.userId] = 1;
+        }
+      }
+    });
+    console.log(completed);
+  }
+});
